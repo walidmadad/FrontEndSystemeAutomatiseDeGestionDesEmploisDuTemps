1af190e10b29ea45a1fd5aeaa2f4c27d9ca18dda
walidmadad <walid.madad123@gmail.com>
2025-01-02 16:29:58 +0100

    Add Tableau de bord

--- src/Componnent/TableauDeBord.jsx
+++ src/Componnent/TableauDeBord.jsx
@@ -1,24 +1,25 @@
 import { useState } from "react"
 import { Route, Routes } from "react-router-dom";
 import Sidebar from "./Sidebar";
-import HomePage from "./HomePage";
+import HomePage from "../pages/HomePage";
 import './TableauDeBord.css'
+import Settings from "../pages/Settings";
 
 export default function TableauDeBord(props){
     const [user, setUser] = useState(props.user)
 
     return (
         
-      <div className="flex h-screen bg-slate-900 text-gray-100 overflow-hidden">
+      <div className="flex h-screen bg-slate-50 text-gray-800 overflow-hidden">
         <div className="fixed inset-0 z-0">
-            <div className="absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80"/>
+            <div className="absolute inset-0 bg-slate-200 opacity-80"/>
             <div className="absolute inset-0 backdrop-blur-sm"/>
         </div>
 
-        <Sidebar/>
+        <Sidebar user={user}/>
         <Routes>
             <Route path="/" element={<HomePage/>}/>
-
+            <Route path="/settings/*" element={<Settings user={user}/>}/>
         </Routes>
       </div> 
     );
