a7b662626dd30c80d13e3ad22be272d99e4b781c
walidmadad <walid.madad123@gmail.com>
2025-02-14 15:31:32 +0100

    update Tableau de bord

--- src/Componnent/TableauDeBord.jsx
+++ src/Componnent/TableauDeBord.jsx
@@ -5,6 +5,7 @@ import Settings from "../pages/Settings";
 import AfficherEmplois from "./VisualisationEmploi/AfficherEmplois";
 import GestionUtilisateur from "./GestionUtilisateur/GestionUtilisateur";
 import AjouterCours from "./AjouterCours/AjouterCours";
+import AfficherEmploisProf from "./VisualisationEmploi/AfficherEmploisProf";
 
 export default function TableauDeBord({ user, onLogout }) {
     return (
@@ -13,11 +14,11 @@ export default function TableauDeBord({ user, onLogout }) {
                 <div className="absolute inset-0 bg-slate-200 opacity-80" />
                 <div className="absolute inset-0 backdrop-blur-sm" />
             </div>
-
             <Sidebar user={user} onLogout={onLogout} />
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/visualisation-emploi" element={<AfficherEmplois />} />
+                <Route path="/visualisation-emploi-prof" element={<AfficherEmploisProf />} />
                 <Route path="/gestion-utilisateur" element={<GestionUtilisateur />} />
                 <Route path="/settings/*" element={<Settings user={user} />} />
                 <Route path="/ajouter-cours" element={<AjouterCours/>}/>
