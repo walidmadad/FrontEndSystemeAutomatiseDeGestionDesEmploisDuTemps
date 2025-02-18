e69b924e44f2cdf69aa9882605ce7818a8c2cfbd
walidmadad <walid.madad123@gmail.com>
2025-02-18 01:45:21 +0100

    update emplois de temps visualisation pour admin

--- src/Componnent/VisualisationEmploi/AfficherEmplois.jsx
+++ src/Componnent/VisualisationEmploi/AfficherEmplois.jsx
@@ -17,6 +17,8 @@ export default function AfficherEmplois() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [message, setMessage] = useState(null);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+
 
   const loadUtilisateurs = async () => {
           setLoading(true);
@@ -67,7 +69,7 @@ export default function AfficherEmplois() {
       const endDate = new Date(`${coursItem.dateDeCours}T${coursItem.finDeCours}`);
   
       return {
-        title: `${coursItem.matiere.nom} (${coursItem.typeDeCours}) - ${coursItem.salle.nom} - ${coursItem.formation.nom}`,
+        title: `${coursItem.matiere.nom} (${coursItem.typeDeCours}) | ${coursItem.salle.nom} | ${coursItem.formation.niveau.nom}-${coursItem.formation.nom}`,
         start: startDate,
         end: endDate,
         colorEvento:'green',
@@ -85,12 +87,33 @@ export default function AfficherEmplois() {
     loadCours(prof)
   };
 
+  const handleEventClick = (event) => {
+    setSelectedEvent(event);
+  };
+  
+
   return (
     <div className="flex-1 overflow-auto relative z-10 bg-gray-100 min-h-screen">
       {/* Header */}
       <Header title="Visualisation des Emplois du Temps" />
 
       <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8 xl:px-20">
+        {/* Enseignant */}
+        <div>
+          <label className='text-sm font-medium text-gray-700'>Nom Enseignant : </label>
+          <input className='ml-2 border rounded-md border-gray-300 bg-white shadow-sm focus:ring-indigo-500 focus:border-indigo-500'
+            value={nomEnseignant}
+            onChange={(e) => {
+            setNomEnseignant(e.target.value)
+            }}></input>
+            <button onClick={ () => {
+            loadUtilisateurs()
+            }} 
+            disabled={nomEnseignant === "" ? "disabled" : ""}
+            className=' bg-blue-500 ml-4 text-white font-semibold py-1 px-4 border rounded-md hover:bg-blue-600 focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 disabled:bg-gray-400'
+            >Rechercher</button>
+        </div>
+
         {/* Sélecteur d'enseignant */}
         <div className="mb-6">
           <label htmlFor="profSelect" className="block text-lg font-medium text-gray-700 mb-2">
@@ -130,8 +153,17 @@ export default function AfficherEmplois() {
               previous: 'Précédent',
               next: 'Suivant',
             }}
+            onSelectEvent={handleEventClick}
           />
         </div>
+        {selectedEvent && (
+          <div className="mt-4 p-4 bg-gray-100 rounded-lg shadow-md">
+            <h3 className="text-xl font-bold text-gray-800">{selectedEvent.title}</h3>
+            <p><strong>Début :</strong> {moment(selectedEvent.start).format('DD/MM/YYYY HH:mm')}</p>
+            <p><strong>Fin :</strong> {moment(selectedEvent.end).format('DD/MM/YYYY HH:mm')}</p>
+          </div>
+        )}
+
       </main>
     </div>
   );
